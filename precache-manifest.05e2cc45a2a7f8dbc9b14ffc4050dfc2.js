a25aad63485de5156559a149b5db589386cb988e
HHKP1 <s.volodymyr82@gmail.com>
2022-04-18 15:43:05 +0300

    initial commit

---
+++ precache-manifest.05e2cc45a2a7f8dbc9b14ffc4050dfc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68fc7b2cecece8abeb6389544a2006e2",
+    "url": "/amic2/index.html"
+  },
+  {
+    "revision": "bf75cac4de4aa1db0952",
+    "url": "/amic2/static/css/2.d71805a1.chunk.css"
+  },
+  {
+    "revision": "55c471f0fa8827f9b456",
+    "url": "/amic2/static/css/main.81d2d364.chunk.css"
+  },
+  {
+    "revision": "bf75cac4de4aa1db0952",
+    "url": "/amic2/static/js/2.c2cb756b.chunk.js"
+  },
+  {
+    "revision": "55c471f0fa8827f9b456",
+    "url": "/amic2/static/js/main.ef3e0919.chunk.js"
+  },
+  {
+    "revision": "9778d778e66e55714ad3",
+    "url": "/amic2/static/js/runtime-main.a7ca63f0.js"
+  },
+  {
+    "revision": "2b6a833a8a3c06283d938b987027221b",
+    "url": "/amic2/static/media/wood-bg-1440x900.2b6a833a.png"
+  }
+]);
